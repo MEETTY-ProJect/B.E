24b353d62a9c52233213dd6c4ead230195e9e1b6
kdh6194 <kdh6194@naver.com>
2025-04-30 21:25:32 +0900

    project init, docs/README.md

---
+++ src/test/java/com/example/meetty/MeettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
