4470dec2255a050640d3ee71086091c2ce97e8ff
SungHeeCheol <107664115+SungHeeCheol@users.noreply.github.com>
2025-05-02 14:04:29 +0900

    signUp complete

---
+++ src/main/java/com/example/meetty/image/entity/UserImageEntity.java
@@ -0,0 +1,30 @@
+package com.example.meetty.image.entity;
+
+import com.example.meetty.auth.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_images")
+public class UserImageEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long userImageId;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity userEntity;
+
+    public UserImageEntity(UserEntity userEntity, String url) {
+        this.userEntity = userEntity;
+        this.url = url;
+    }
+}
