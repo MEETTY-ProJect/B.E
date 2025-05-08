6c33889acabb3b81714f5a8e5389b9de68ac72c7
bbizin1004 <bbizin1004@korea.com>
2025-05-08 15:39:17 +0900

    chat push 테스트3

--- src/main/java/com/example/meetty/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/meetty/chat/repository/ChatMessageRepository.java
@@ -1,7 +1,4 @@
 package com.example.meetty.chat.repository;
 
-import com.example.meetty.chat.entity.ChatMessages;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface ChatMessageRepository {
 }
