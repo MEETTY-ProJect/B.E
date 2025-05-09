ef37f511aa1862099012b5383b87fbc5c8c242bc
bbizin1004 <bbizin1004@korea.com>
2025-05-09 13:05:35 +0900

    채팅 API

---
+++ src/main/java/com/example/meetty/chat/controller/ChatMessageController.java
@@ -0,0 +1,43 @@
+package com.example.meetty.chat.controller;
+
+import com.example.meetty.chat.dto.ChatMessageRequestDto;
+import com.example.meetty.chat.dto.ChatMessageResponseDto;
+import com.example.meetty.chat.service.ChatMessageService;
+import com.example.meetty.global.config.auth.CustomUserDetails;
+import com.example.meetty.global.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/chat/rooms")
+@RequiredArgsConstructor
+@Tag(name = "채팅관련 API")
+public class ChatMessageController {
+
+    private final ChatMessageService chatMessageService;
+
+    @Operation(summary = "채팅조회" , description = "채팅을 리미트 수만큼 최근 목록 조회, 스크롤 조회 가능")
+    @GetMapping("/{roomId}/messages")
+    public ApiResponse<List<ChatMessageResponseDto>> getMessages(@PathVariable Long roomId,
+                                                                @RequestParam(required = false) Long lastMessageId,
+                                                                @RequestParam(defaultValue = "30") int limit) {
+        List<ChatMessageResponseDto> messages = chatMessageService.getChatMessages(roomId, lastMessageId, limit);
+        return ApiResponse.success(messages);
+    }
+
+    @Operation(summary = "채팅쓰기" , description = "채팅을 서버에 저장하는 API")
+    @PostMapping("/{roomId}/messages")
+    public ApiResponse<Void> saveMessage(@PathVariable Long roomId,
+                                            @RequestBody ChatMessageRequestDto request,
+                                            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        Long userId = userDetails.getUserId();
+        chatMessageService.saveMessage(roomId,userId, request.getMessage());
+        return ApiResponse.success(null);
+    }
+}
--- src/main/java/com/example/meetty/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/meetty/chat/repository/ChatMessageRepository.java
@@ -1,4 +1,7 @@
 package com.example.meetty.chat.repository;
 
-public interface ChatMessageRepository {
+import com.example.meetty.chat.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long>, ChatMessageRepositoryCustom {
 }
